b9a3afdba58ea56eb3a1b0ab101ab32b252acbe7
DziewaAngelika <dziewa@vp.pl>
2020-04-19 16:04:06 +0200

    Usunięcie zbędnych kodów z Matlaba i Pythona

--- 3dunet/funkcje_matlab/genh5.m
+++
@@ -1,7 +0,0 @@
-a=dicomread('Series_80452_CORONALS.dcm');
-b=dicomread('Series_mask.dcm');
-im1=squeeze(a(:,:,1,:));
-mask1=squeeze(b(:,:,1,:));
-% save_hdf5(im1, mask1, 'uterus.h5');
-[raw, label] = load_hdf5('uterus.h5');
-imshow(imfuse(raw(:,:,30),label(:,:,30)))
